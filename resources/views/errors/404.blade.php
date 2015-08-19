fafd669cf8cb75ff2ebdab1b99a234842d13dd2c
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-08-19 11:37:03 +0700

    errors

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,4 @@
+@extends('app') @section('title') Error @endsection
+@section('content')
+	<div class="alert alert-warning" role="alert"><strong>Halaman tidak ditemukan.</strong> Periksa kembali alamat yang anda tuju atau hubungi admin.</div>
+@endsection
